fa37425b1e0460692b637f05b4e5baba02bcb65e
Viktoria <viktoriia.andreeva@gmail.com>
2024-04-26 10:25:19 +0400

    hz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HotelListComponent } from './component/hotel-list/hotel-list.component';
+import { HotelComponent } from './component/hotel/hotel.component';
+import { ServiceListComponent } from './component/service-list/service-list.component';
+import { CreateVisitComponent } from './component/create-visit/create-visit.component';
+import { PetListComponent } from './component/pet-list/pet-list.component';
+import { CreateOrUpdatePetComponent } from './component/create-or-update-pet/create-or-update-pet.component';
+import { CreateOrUpdateServiceComponent } from './component/create-or-update-service/create-or-update-service.component';
+import { VisitListComponent } from './component/visit-list/visit-list.component';
+import { CreateOrUpdateHotelComponent } from './component/create-or-update-hotel/create-or-update-hotel.component';
+import { PersonalAccountComponent } from './component/personal-account/personal-account.component';
+import { HotelTableComponent } from './component/hotel-table/hotel-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'hotels', component: HotelListComponent},
+  {path: 'hotels/:id', component: HotelComponent},
+  {path: 'hotels/:id/services', component: ServiceListComponent},
+  {path: 'visits/new', component: CreateVisitComponent},
+  {path: 'pets', component: PetListComponent},
+  {path: 'pets/new', component: CreateOrUpdatePetComponent},
+  {path: 'pets/:id/edit', component: CreateOrUpdatePetComponent},
+  {path: 'services', component: ServiceListComponent},
+  {path: 'services/new', component: CreateOrUpdateServiceComponent},
+  {path: 'services/:id/edit', component: CreateOrUpdateServiceComponent},
+  {path: 'visits', component: VisitListComponent},
+  {path: 'visits/new', component: CreateVisitComponent},
+  {path: 'visits/:id/edit', component: CreateVisitComponent},
+  {path: 'hotels/new', component: CreateOrUpdateHotelComponent},
+  {path: 'hotels/:id/edit', component: CreateOrUpdateHotelComponent},
+  {path: 'account', component: PersonalAccountComponent},
+  {path: 'z', component: HotelTableComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,63 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HotelComponent } from './component/hotel/hotel.component';
+import { HotelListComponent } from './component/hotel-list/hotel-list.component';
+import { ServiceListComponent } from './component/service-list/service-list.component';
+import { CreateVisitComponent } from './component/create-visit/create-visit.component';
+
+
+import { NgSelectModule } from "@ng-select/ng-select"; 
+import { FormsModule } from '@angular/forms';
+import { PetListComponent } from './component/pet-list/pet-list.component';
+import { CommonModule, NgIf } from '@angular/common';
+import { CreateOrUpdatePetComponent } from './component/create-or-update-pet/create-or-update-pet.component';
+import { CreateOrUpdateServiceComponent } from './component/create-or-update-service/create-or-update-service.component';
+import { VisitListComponent } from './component/visit-list/visit-list.component';
+import { DpDatePickerModule } from 'ng2-date-picker';
+import { CreateOrUpdateHotelComponent } from './component/create-or-update-hotel/create-or-update-hotel.component';
+import { PersonalAccountComponent } from './component/personal-account/personal-account.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HotelTableComponent } from './component/hotel-table/hotel-table.component';
+import { ServiceTableComponent } from './component/service-table/service-table.component';
+import { VisitTableComponent } from './component/visit-table/visit-table.component';
+import { UserTableComponent } from './component/user-table/user-table.component';
+import { PetTableComponent } from './component/pet-table/pet-table.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelComponent,
+    HotelListComponent,
+    ServiceListComponent,
+    CreateVisitComponent,
+    PetListComponent,
+    CreateOrUpdatePetComponent,
+    CreateOrUpdateServiceComponent,
+    VisitListComponent,
+    CreateOrUpdateHotelComponent,
+    PersonalAccountComponent,
+    HotelTableComponent,
+    ServiceTableComponent,
+    VisitTableComponent,
+    UserTableComponent,
+    PetTableComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgSelectModule,
+    FormsModule,
+    NgIf,
+    DpDatePickerModule,
+    NgbModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
